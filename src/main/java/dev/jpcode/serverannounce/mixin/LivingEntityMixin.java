a8698d95edda40f8d560017e17136a529476eb41
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-07-10 11:23:17 -0400

    Add death messages for named entities... Update config.yml Remove some checkstyle.xml requirements.

--- src/main/java/dev/jpcode/serverannounce/mixin/LivingEntityMixin.java
+++ src/main/java/dev/jpcode/serverannounce/mixin/LivingEntityMixin.java
@@ -1,24 +1,16 @@
 package dev.jpcode.serverannounce.mixin;
 
-import net.minecraft.entity.LivingEntity;
-
-import net.minecraft.entity.damage.DamageSource;
-
-import net.minecraft.network.MessageType;
-import net.minecraft.text.Text;
-
-import net.minecraft.util.Util;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import net.minecraft.client.gui.screen.TitleScreen;
-
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import static dev.jpcode.serverannounce.ServerAnnounce.LOGGER;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.network.MessageType;
+import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin
@@ -26,6 +18,7 @@ public class LivingEntityMixin
     /**
      * Injects into the beginning of the onDeath method and sends a chat death message if the entity has a custom name.
      *
+     * @param source cause of death
      * @param info callback info
      */
     @Inject(method = "onDeath",
@@ -33,7 +26,7 @@ public class LivingEntityMixin
         locals = LocalCapture.CAPTURE_FAILSOFT
     )
     private void onDeath(DamageSource source, CallbackInfo info) {
-        LivingEntity self = ((LivingEntity)(Object)this);
+        LivingEntity self = (LivingEntity)(Object)this;
         if (self.hasCustomName()) {
             Text text = self.getDamageTracker().getDeathMessage();
             self.getServer().getPlayerManager().broadcastChatMessage(text, MessageType.SYSTEM, Util.NIL_UUID);
